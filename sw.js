50d2cbd7780d55cdf2c7273b762b690f17fe7979
虚の时月 <63053541+invoidstar@users.noreply.github.com>
2026-09-27 00:44:43 +0800

    Add Busuanzi site traffic counters

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='plush-color-matcher-v3.8';
+const CACHE='plush-color-matcher-v3.8.1';
 const ASSETS=[
   './','./index.html','./palette.html','./manifest.webmanifest','./icon.svg',
-  './css/base.css','./css/theme.css','./css/features.css','./css/palette.css','./css/announcement.css',
+  './css/base.css','./css/theme.css','./css/features.css','./css/palette.css','./css/announcement.css','./css/traffic.css',
   './js/core/color.js','./js/app.js','./js/palette-gallery.js','./js/announcements.js',
   './data/announcements.js','./data/qqtq-480.js','./data/hanter-620.js','./data/alice-1680.js','./guide.html','./css/guide.css'
 ];
